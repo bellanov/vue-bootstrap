4f245de6969361ca316b512ef5e163a1f5b372dc
Bellanov Apilli <BellanovApilli@gmail.com>
2022-05-24 00:10:15 -0400

    Scripts Templates

---
+++ scripts/deploy_artifact.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+# Deploy a generic artifact to Google Cloud Platform (GCP).
+
+# Pull Release Artifact (*.zip)
+# Pull from GitHub Artifact Repository.
+
+# Push to GCP
+# Copy into relevant project's (dev, qa, etc.) release bucket.
