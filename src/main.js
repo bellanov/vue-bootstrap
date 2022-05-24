27383e0786fe49ce63aaffadb36ac838125368c8
Bellanov Apilli <BellanovApilli@gmail.com>
2022-05-23 23:31:25 -0400

    Initial Integration Complete

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
 // Bootstrap
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap"
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
 
-createApp(App).mount('#app')
+//Initialize
+createApp(App).mount('#app');
