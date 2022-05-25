090aee20fbcd2c28bfc0fb793f1eb14ee3e743cc
Bellanov Apilli <BellanovApilli@gmail.com>
2022-05-24 22:13:56 -0400

    Initial push to GCP

--- scripts/deploy_container.sh
+++ scripts/deploy_container.sh
@@ -4,9 +4,15 @@
 
 # Pull Release Image
 # Pull from GitHub Artifact Repository
+docker pull ghcr.io/bellanov/vue-bootstrap:v1.0.0
 
 # Tag Release Image
 # Tag Release image for GCP's Artifact Repository
+docker tag vue-bootstrap:v1.0.0 gcr.io/vue-bootstrap-dev/releases/vue-bootstrap:v1.0.0
 
 # Push to GCP
 # Push container to GCP's Artifact Repository
+docker push gcr.io/vue-bootstrap-dev/releases/vue-bootstrap:v1.0.0
+
+# Terraform
+# Update terraform to point to the new version of the artifact.
