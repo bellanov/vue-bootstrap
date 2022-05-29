215d943c4378aa41fa21bbbfac0fafe22a10a042
Bellanov Apilli <BellanovApilli@gmail.com>
2022-05-29 15:27:57 -0400

    QA Checkpoint

--- scripts/deploy_artifact.sh
+++ scripts/deploy_artifact.sh
@@ -4,7 +4,7 @@
 
 VERSION=v1.1.0
 DEV_RELEASE_BUCKET=releases-ifn7a7levvrxumj
-# QA_RELEASE_BUCKET=releases-ifn7a7levvrxumj
+QA_RELEASE_BUCKET=releases-t6g3wiipaxnjgyd
 # PROD_RELEASE_BUCKET=releases-ifn7a7levvrxumj
 
 # Pull Release Artifact (*.zip)
@@ -14,6 +14,7 @@ wget https://github.com/bellanov/vue-bootstrap/archive/refs/tags/$VERSION.zip
 # Push to GCP
 # Copy into relevant project's (dev, qa, etc.) release bucket.
 gsutil cp $VERSION.zip gs://$DEV_RELEASE_BUCKET
+gsutil cp $VERSION.zip gs://$QA_RELEASE_BUCKET
 
 
 # Terraform
--- scripts/deploy_container.sh
+++ scripts/deploy_container.sh
@@ -8,6 +8,7 @@ VERSION=v1.1.0
 
 # Dev / QA / Prod
 DEV_RELEASE_REGISTRY=us-docker.pkg.dev/vue-bootstrap-dev/releases
+QA_RELEASE_REGISTRY=us-docker.pkg.dev/vue-bootstrap-qa/releases
 
 # Pull Release Image
 # Pull from GitHub Artifact Repository
@@ -16,10 +17,12 @@ docker pull $GITHUB_RELEASE_REPOSITORY:$VERSION
 # Tag Release Image
 # Tag Release image for GCP's Artifact Repository
 docker tag $GITHUB_RELEASE_REPOSITORY:$VERSION $DEV_RELEASE_REGISTRY/vue-bootstrap:$VERSION
+docker tag $GITHUB_RELEASE_REPOSITORY:$VERSION $QA_RELEASE_REGISTRY/vue-bootstrap:$VERSION
 
 # Push to GCP
 # Push container to GCP's Artifact Repository
 docker push $DEV_RELEASE_REGISTRY/vue-bootstrap:$VERSION
+docker push $QA_RELEASE_REGISTRY/vue-bootstrap:$VERSION
 
 # Terraform
 # Update terraform to point to the new version of the artifact.
